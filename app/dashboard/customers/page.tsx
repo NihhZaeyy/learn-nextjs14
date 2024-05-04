ac0a43b00be6e5aad22f6ca5e4ec7ca02fd87333
NihhZaeyy <nizaralmas11@gmail.com>
2024-05-04 20:51:56 +0300

    styles, router, layouting

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <p>CUSTOMERS PAGE</p>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <p>INVOICES PAGE</p>
+    </div>
+  );
+};
+
+export default page;
